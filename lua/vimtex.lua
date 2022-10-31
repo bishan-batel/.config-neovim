e03e25859d93a7043ac14058c735114ba884ba38
bishan <bishan.batel@gmail.com>
2022-10-30 22:07:29 -0500

    Tab controls

--- lua/vimtex.lua
+++ lua/vimtex.lua
@@ -1,10 +1,10 @@
 vim.cmd [[filetype plugin indent on]]
 
+vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.vimtex_view_method="zathura"
 vim.g.vimtex_view_general_viewer="zathura"
 vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
-vim.g.vimtex_compiler_method = 'latexrun'
 
-vim.keymap.set("n", "\\", "", {})
+--vim.keymap.set("n", "\\", "", {})
 vim.g.tex_conceal="abdmg"
 
